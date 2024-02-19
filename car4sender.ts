fb4e0c37cd28f82a83e2f65af5a93f556eff3265
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-02-19 21:33:37 +0100

    Aktualisiere pxt.json, main.blocks, main.ts, car4sender.ts

--- car4sender.ts
+++ car4sender.ts
@@ -7,8 +7,8 @@ namespace car4sender
 
     const n_Simulator: boolean = ("€".charCodeAt(0) == 8364) // true, wenn der Code im Simulator läuft
     let n_Buffer19 = Buffer.create(19) // wird gesendet mit radio.sendBuffer
-    let n_x: number, n_y: number, n_xmotor: number, n_yservo: number
-    let n_xmin: number, n_xmax: number, n_ymin: number, n_ymax: number
+    let n_x: number, n_y: number, n_xMotor: number, n_yServo: number
+    let n_xmin = 128, n_xmax = 128, n_ymin = 128, n_ymax = 128
 
     // ========== group="beim Start"
 
@@ -18,17 +18,16 @@ namespace car4sender
 
     //% inlineInputMode=inline 
     export function beimStart(funkgruppe: number) {
-
         // Parameter
         radio.setGroup(funkgruppe)
         radio.setTransmitPower(7)
-
-
     }
 
     //% group="Bluetooth senden" color=#E3008C
     //% block="Datenpaket senden" weight=9
     export function sendBuffer19() {
+        n_Buffer19.setUint8(0,n_xMotor)
+        n_Buffer19.setUint8(1,n_yServo)
         radio.sendBuffer(n_Buffer19)
     }
 
@@ -39,15 +38,15 @@ namespace car4sender
         switch (pJoystickValue) {
             case eJoystickValue.x: return n_x
             case eJoystickValue.y: return n_y
-            case eJoystickValue.motor: return n_xmotor
-            case eJoystickValue.servo: return n_yservo
+            case eJoystickValue.motor: return n_xMotor
+            case eJoystickValue.servo: return n_yServo
             default: return 0
         }
     }
 
     //% group="Joystick"
-    //% block="Joystick Qwiic" weight=8
-    export function qwiicJoystick() {
+    //% block="Joystick Qwiic einlesen" weight=8
+    export function joystickQwiic() {
         if (pins.i2cWriteBuffer(i2cqwiicJoystick_x20, Buffer.fromArray([3]), true) != 0)
             return false
         else {
@@ -55,13 +54,16 @@ namespace car4sender
             n_x = bu.getUint8(0)
             n_y = bu.getUint8(2)
             // Motor
-            if (between(n_x, 124, 132)) n_xmotor = 128
-            else n_xmotor = n_x
+            if (between(n_x, 124, 132)) n_xMotor = 128
+            else n_xMotor = n_x
             // Servo
-            if (between(n_y, 122, 134)) n_yservo = 90 // Ruhestellung soll 128 ist auf 128 = 90° anpassen
-            else if (n_y < 20) n_yservo = 135 // Werte < 20 wie 0 behandeln (max links)
-            else if (n_y > 235) n_yservo = 45 // Werte > 235 wie 255 behandeln (max rechts)
-            else n_yservo = Math.map(n_y, 20, 235, 134, 46) // Werte von 32 bis 991 auf 46° bis 134° verteilen
+            if (between(n_y, 122, 134)) n_yServo = 90 // Ruhestellung soll 128 ist auf 128 = 90° anpassen
+            else if (n_y < 20) n_yServo = 135 // Werte < 20 wie 0 behandeln (max links)
+            else if (n_y > 235) n_yServo = 45 // Werte > 235 wie 255 behandeln (max rechts)
+            else n_yServo = Math.map(n_y, 20, 235, 134, 46) // Werte von 32 bis 991 auf 46° bis 134° verteilen
+
+            n_yServo = Math.round(n_yServo)
+            //n_yservo = Math.round(n_yservo / 3 - 14) // 0=31 90=16 135=1
             return true
         }
     }
@@ -73,12 +75,12 @@ namespace car4sender
             case eXY.x: {
                 if (n_x > 0 && n_x < n_xmin) n_xmin = n_x
                 else if (n_x < 255 && n_x > n_xmax) n_xmax = n_x
-                return format(n_x, 4, eAlign.right) + format(n_xmin, 4, eAlign.right) + format(n_xmax, 4, eAlign.right) + format(n_xmotor, 4, eAlign.right)
+                return format(n_x, 4, eAlign.right) + format(n_xmin, 4, eAlign.right) + format(n_xmax, 4, eAlign.right) + format(n_xMotor, 4, eAlign.right)
             }
             case eXY.y: {
                 if (n_y > 0 && n_y < n_ymin) n_ymin = n_y
                 else if (n_y < 255 && n_y > n_ymax) n_ymax = n_y
-                return format(n_y, 4, eAlign.right) + format(n_ymin, 4, eAlign.right) + format(n_ymax, 4, eAlign.right) + format(n_yservo, 4, eAlign.right)
+                return format(n_y, 4, eAlign.right) + format(n_ymin, 4, eAlign.right) + format(n_ymax, 4, eAlign.right) + format(n_yServo, 4, eAlign.right)
             }
             default: return ""
         }
@@ -87,6 +89,42 @@ namespace car4sender
 
 
     // ========== advanced=true ==========
+
+
+    // ========== group="ein Bit aus Number (bis 32 Bit) lesen und ändern" advanced=true
+
+    //% group="ein Bit aus Number (bis 32 Bit) lesen und ändern" advanced=true
+    //% block="Zahl %pInt getBit 2** %exp" weight=4
+    //% exp.min=0 exp.max=31
+    export function getBit(pInt: number, exp: number): boolean {
+        exp = Math.trunc(exp)
+        if (between(exp, 0, 63))
+            return (pInt & 2 ** exp) != 0
+        else
+            return false
+    }
+
+    //% group="ein Bit aus Number (bis 32 Bit) lesen und ändern" advanced=true
+    //% block="Zahl %pInt setBit 2** %exp %pBit" weight=2
+    //% exp.min=0 exp.max=31
+    export function setBit(pInt: number, exp: number, pBit: boolean) {
+        exp = Math.trunc(exp)
+        if (between(exp, 0, 63)) {
+            if (pBit)
+                return pInt | 2 ** exp
+            else
+                return pInt & ~(2 ** exp)
+        } else
+            return pInt
+    }
+
+    //% group="ein Bit aus Number (bis 32 Bit) lesen und ändern" advanced=true
+    //% block="Buffer offset %pOffset setBit 2** %exp %pBit" weight=1
+    //% pOffset.defl=3
+    //% exp.min=0 exp.max=7
+    export function setBitBuffer(pOffset: number, exp: number, pBit: boolean) {
+        n_Buffer19.setUint8(pOffset, setBit(n_Buffer19.getUint8(pOffset), exp, pBit))
+    }
 
     // ========== group="Logik" advanced=true
 
--- main.ts
+++ main.ts
@@ -10,9 +10,11 @@ loops.everyInterval(500, function () {
     } else {
         basic.setLedColor(0x007fff)
     }
-    if (car4sender.qwiicJoystick()) {
-        lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 0, 0, 15, car4sender.joystickValues(car4sender.eJoystickValue.x))
-        lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 0, 0, 15, car4sender.joystickValues(car4sender.eJoystickValue.y))
+    if (car4sender.joystickQwiic()) {
+        lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 0, 0, 15, car4sender.minmaxZeile(car4sender.eXY.x))
+        lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 1, 0, 15, car4sender.minmaxZeile(car4sender.eXY.y))
     }
+    car4sender.setBitBuffer(3, 7, bMotorPower)
+    car4sender.sendBuffer19()
     basic.turnRgbLedOff()
 })
