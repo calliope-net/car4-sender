4590dfbe547437f42c1f58f0af7ce59801184679
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-02-25 11:22:09 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,7 +5,6 @@ input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
     bMotorPower = !(bMotorPower)
 })
 let bMotorPower = false
-lcd16x2rgb.initLCD(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E))
 car4sender.beimStart(240)
 bMotorPower = false
 loops.everyInterval(750, function () {
@@ -15,8 +14,7 @@ loops.everyInterval(750, function () {
         basic.setLedColor(65536 * 7)
     }
     if (car4sender.joystickQwiic()) {
-        lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 0, 0, 15, car4sender.minmaxZeile(car4sender.eXY.x))
-        lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 1, 0, 15, car4sender.minmaxZeile(car4sender.eXY.y))
+    	
     }
     car4sender.sendBuffer_setUint8(car4sender.eBufferOffset.b0_Motor, car4sender.joystickValues(car4sender.eJoystickValue.motor))
     car4sender.sendBuffer_setUint8(car4sender.eBufferOffset.b1_Servo, car4sender.joystickValues(car4sender.eJoystickValue.servo))
